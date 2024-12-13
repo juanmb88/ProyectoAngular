bdad1c21ad529d072b7ea5b7f882bdbe115231d8
Juan-Balugano <juanbalugano@gmail.com>
2024-12-13 20:01:06 -0300

    Mi mensaje descriptivo del cambio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentsPageComponent } from './pages/students-page/students-page.component';
+import { HeaderComponent } from './components/header/header.component';
+import { TitleComponent } from './components/title/title.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentsPageComponent,
+    HeaderComponent,
+    TitleComponent,
+    SidebarComponent,
+
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/title/title.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-title',
+  standalone: false,
+  
+  template: `
+    <p class="fs-1">
+      TITULO
+    </p>
+  `,
+  styles: ``
+})
+export class TitleComponent {
+
+}
